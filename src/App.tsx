005a5fa54c2aa3e635806551f0a2035e824ff682
tejava317 <frogjj@kaist.ac.kr>
2025-01-20 19:36:33 +0900

    feat: Implement Google login in local env

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-// import LoginPage from "./pages/LoginPage";
+import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
 
-export default function App() {
+const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
   );
-}
+};
+
+export default App;
